cc78344407787cff1f0dde68d5662ceecce631cd
omarox98 <omaraizab98@gmail.com>
2019-02-21 14:16:39 +0100

    ajout des codes a script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var nom = prompt("entrer ton nom");
-alert("Bonjour " + nom);
+var nom = prompt("entrer votre nom");
+var prenom = prompt("entrer votre prenom");
+var age = prompt("entrer votre age");
+var ageNum = parseInt(age);
+var message;
+if(ageNum >= 18)
+{
+	message = "tu es majeure";
+}
+else
+{
+	message = "tu es mineure";
+}
+alert("Bonjour " + nom + " " + prenom + " " + message);
